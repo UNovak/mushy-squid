cd3d4e9fb70a79ac20fd7fac88998bb4262a0c15
UNovak <88665587+UNovak@users.noreply.github.com>
2026-01-01 17:37:14 +0100

    feat: implement data models

---
+++ src/utils/models.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Node:
+  """
+    x: int
+    y: int
+    demand: int
+  """
+
+  x: int
+  y: int
+  demand: int = 0
+
+
+@dataclass
+class ProblemData:
+  """
+  nodes: dict[node_id,[id,x,y,demand]]
+  depot_id: int
+  truck_capacity: int
+  dimension: int
+  dataset: str
+  edge_type: str
+  """
+
+  nodes: dict[int,Node]
+  depot_id: int
+  truck_capacity: int
+  dimension: int
+  dataset: str
+  edge_type: str
