4a737853b588826ccb29bbffc33f73e5046c3ec5
UNovak <88665587+UNovak@users.noreply.github.com>
2026-01-26 03:49:09 +0100

    refactor: implement running GA in `src/main.py`

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 import utils.parser as parser
+from algorithms import genetic
 
 print("CVRP solver")
 
 data = parser.parse_file("./data/E-n22-k4.vrp")
 print(data)
+
+# run the algorithms
+genetic.run(data)
