4fa1d348f6eeb715fe9a3835c4afb407b626f531
UNovak <88665587+UNovak@users.noreply.github.com>
2026-01-28 01:21:54 +0100

    refactor: update GA to work with the `Data` model

--- src/algorithms/genetic.py
+++ src/algorithms/genetic.py
@@ -1,47 +1,42 @@
 import math
 import random
 
-from utils.models import ProblemData
+from utils.models import Data
 
 
-def run(data: ProblemData, dist_matrix):
+def run(data: Data):
     # extract the needed data
-    nodes = data.nodes
-    capacity = data.truck_capacity
+    capacity = data.capacity
     depot_id = data.depot_id
 
-    # generate a random population
     def init():
         """returns a randomly generated population"""
         # scale the population size based on input
         population_size = math.floor((data.dimension - 1) / 10) * 2
 
-        # generate a list of all node ids
-        ids = [k for k in nodes.keys() if k != depot_id]
         population = []
 
         for _ in range(population_size):
-            unvisited = set(ids)
+            unvisited = set(data.ids)  # get all customer node_ids
             routes = []
 
             while unvisited:
                 current_capacity = capacity
                 possible_customer = list(unvisited)
-                route = []
+                route = []  # path of one vehicle
 
                 while possible_customer:
-                    id = random.choice(possible_customer)
-                    current_capacity -= nodes[id].demand
-                    route.append(id)
-                    unvisited.remove(id)
+                    id = random.choice(possible_customer)  # pick a random node
+                    current_capacity -= data.demands[id]  # update truck capacity
+                    route.append(id)  # add id to path
+                    unvisited.remove(id)  # mark node as visited, remove it from set
 
                     # update possible customers based on unvisited nodes and remaining truck capacity
                     possible_customer = [
-                        id for id in unvisited if nodes[id].demand <= current_capacity
+                        id for id in unvisited if data.demands[id] <= current_capacity
                     ]
 
-                # no possible next customer
-                current_capacity = capacity
+                # unvisited nodes, no possible customer
                 routes.append(route)
 
             # all customers visited
