4869c42e3d4c73ce0c3788f73f9791615a33af53
UNovak <88665587+UNovak@users.noreply.github.com>
2026-01-28 01:58:27 +0100

    feat: add distance matrix generation helper

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,6 +1,6 @@
 import math
 
-from utils.models import Node
+from utils.models import Node, ProblemData
 
 
 def euc(i: Node, j: Node):
@@ -10,3 +10,18 @@ def euc(i: Node, j: Node):
     yd = i.y - j.y  # y coordinate
     dist = round(math.sqrt(xd * xd + yd * yd))
     return dist
+
+
+def all_distances(data: ProblemData) -> dict[int, dict[int, int]]:
+    """returns a dictionary of all distances"""
+    ids = list(data.nodes.keys())
+    result = {}
+
+    for key in ids:
+        tmp = {}
+        for id in ids:
+            tmp[id] = euc(data.nodes[id], data.nodes[key])
+
+        result[key] = tmp
+
+    return result
