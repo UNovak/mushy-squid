9124f25d66c33839c941834077b5454fd696fe38
UNovak <88665587+UNovak@users.noreply.github.com>
2026-02-01 15:47:21 +0100

    feat: implement validate_route
    - Implement checking for duplicates global and local.
    - implement checking capacity constraint.
    - Implement calculating the cost of the valid segment.
    - Implement calculating the remaining capacity of the valid segment.

---
+++ src/utils/fix_routes.py
@@ -0,0 +1,26 @@
+from utils.models import Data
+
+
+def validate_route(route, visited, data: Data) -> tuple[bool, int, int, int]:
+    locals: set[int] = set()
+    capacity = data.capacity  # 100
+    cost = 0
+    prev = data.depot_id
+    for index, id in enumerate(route):
+        if capacity - data.demands[id] < 0:
+            return False, cost, capacity, index
+
+        if id in visited:
+            return False, cost, capacity, index
+
+        if id in locals:
+            return False, cost, capacity, index
+
+        # no errors, update values
+        locals.add(id)
+        capacity -= data.demands[id].item()
+        cost += data.distance[prev, id].item()
+        prev = id
+
+    cost += data.distance[prev, data.depot_id]
+    return True, cost, capacity, len(route)
