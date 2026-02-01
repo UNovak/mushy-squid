f486dbec4ba9be45ff36ee4bf83deea7438a5751
UNovak <88665587+UNovak@users.noreply.github.com>
2026-02-01 03:38:52 +0100

    feat: implement a helper for displaying types

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,8 +1,9 @@
 import math
+import random
 
 import numpy as np
 
-from utils.models import Data
+from utils.models import Data, Solution
 
 
 def euc(a: tuple[int, int], b: tuple[int, int]):
@@ -39,3 +40,42 @@ def route_cost(data: Data, route: list[int]) -> int:
     cost += int(data.distance[prev, data.depot_id])
     return cost
 
+
+def type_print(data: Data, s: Solution) -> None:
+    """prints all of the variables and the responding types"""
+
+    id1 = data.ids[0]
+    id2 = data.ids[-1]
+    id = random.choice(data.ids)
+    x, y = data.nodes[id]
+    demand = data.demands[id]
+    distance = data.distance[id1, id2]
+    calculated_demand = 99
+    calculated_distance = 99
+
+    print(
+        " \n\n ------------------ TYPES -------------------------  \n\n",
+        f"random id: {id}, ids[0]: {id1}, ids[-1]: {id2}\n",
+        f"nodes -> type(nodes): {type(data.nodes)}, node: {data.nodes[id]} {type(data.nodes[id])}\n",
+        f"x:{x} {type(x)}, y:{y} {type(y)}\n",
+        f"depot_id -> depot_id: {data.depot_id} {type(data.depot_id)}\n",
+        f"euc -> euc_result: {euc(data.nodes[1], data.nodes[2])} {type(euc(data.nodes[1], data.nodes[2]))}\n",
+        f"solution -> uuid: {s.id} {type(s.id)}\n",
+        f"solution -> cost: {s.cost} {type(s.cost)}\n",
+        f"solution -> routes: {s.routes} {type(s.routes)}\n",
+        f"solution -> route: {s.routes[0]} {type(s.routes[0])}\n",
+        f"solution -> route_id: {s.routes[0][0]} {type(s.routes[0][0])} \n",
+        f"demands -> type(demands): {type(data.demands)}, demand:{demand}, {type(demand)}\n",
+        f"distance -> type(distance): {type(data.distance)} distance: {distance} {type(distance)}\n",
+        f"capacity -> capacity: {data.capacity} {type(data.capacity)}\n",
+        f"calculated -> demand before: {calculated_demand} {type(calculated_demand)}\n",
+        f"calculated -> distance before: {calculated_distance} {type(calculated_distance)}",
+    )
+
+    calculated_demand += demand
+    calculated_distance += distance
+
+    print(
+        f"calculated_demand += demand -> calculated_demand: {calculated_demand} {type(calculated_demand)}\n",
+        f"calculated_distance += distance -> calculated_distance: {calculated_distance} {type(calculated_distance)}\n",
+    )
