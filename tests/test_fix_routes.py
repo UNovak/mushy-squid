dd93d149d6be15706b67841c5c02ddaed5604e9e
UNovak <88665587+UNovak@users.noreply.github.com>
2026-02-01 16:18:29 +0100

    test: add tests for validate_route function

--- tests/test_fix_routes.py
+++ tests/test_fix_routes.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytest
 
+from utils.fix_routes import validate_route
 from utils.models import Data
 
 
@@ -37,3 +38,56 @@ def sample_data() -> Data:
             [0, 3, 2, 3, 2, 5, 0],
         ]),
     )
+
+
+def test_route_validation(sample_data: Data):
+    visited = {}
+
+    # valid
+    valid = [2, 6, 5]
+    status, _, cap, idx = validate_route(valid, visited, sample_data)
+    assert status is True
+    assert idx == len(valid)
+    assert cap == 0
+
+    # local duplicate
+    visited = {}
+    local_dup = [6, 5, 6]
+    status, _, _, idx = validate_route(local_dup, visited, sample_data)
+    assert status is False
+    assert idx == 2
+
+    # fault at index zero
+    visited = {6}
+    global_dup = [6, 2, 5]
+    status, cost, cap, idx = validate_route(global_dup, visited, sample_data)
+    assert status is False
+    assert idx == 0
+    assert cost == 0
+    assert cap == sample_data.capacity
+
+    # fault in the middle
+    visited = {6}
+    global_dup = [2, 6, 5]
+    status, cost, cap, idx = validate_route(global_dup, visited, sample_data)
+    assert status is False
+    assert idx == 1
+    assert cost == 3
+    assert cap == 60
+
+    # fault at last index
+    visited = {6}
+    global_dup = [2, 5, 6]
+    status, cost, cap, idx = validate_route(global_dup, visited, sample_data)
+    assert status is False
+    assert idx == 2
+    assert cost == 10
+    assert cap == 20
+
+    # exceeded capacity
+    visited = {}
+    cap_limit = [2, 5, 3]
+    status, _, cap, idx = validate_route(cap_limit, visited, sample_data)
+    assert status is False
+    assert idx == 2
+    assert cap == 20
