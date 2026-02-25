7a7e86f315fd827ad5b92ad292065a4376cbd79f
UNovak <88665587+UNovak@users.noreply.github.com>
2026-02-25 03:37:06 +0100

    refactor(GA): modularize algorithm structure
    - Split init into `generate_individual` and logic calling it in run.
    - Integrate `fix_routes` utility.
    - Update the population structure to store tuples of (cost, routes).
    - Add placeholders for `mutate`, `crossover`, `tournament_selection`.

--- src/algorithms/genetic.py
+++ src/algorithms/genetic.py
@@ -1,47 +1,57 @@
 import math
 import random
 
+from utils.fix_routes import fix_routes
 from utils.models import Data
 
 
+def generate_individual(data: Data) -> list[list[int]]:
+    """returns one randomly generated solution"""
+    unvisited = set(data.ids)  # get all customer node_ids
+    routes: list[list[int]] = []
+
+    while unvisited:
+        current_capacity = data.capacity
+        possible_customer = list(unvisited)
+        route: list[int] = []  # path of one vehicle
+
+        while possible_customer:
+            id = random.choice(possible_customer)  # pick a random node
+            current_capacity -= data.demands[id]  # update truck capacity
+            route.append(id)  # add id to path
+            unvisited.remove(id)  # mark node as visited, remove it from set
+
+            # update possible customers based on unvisited nodes and remaining truck capacity
+            possible_customer = [id for id in unvisited if data.demands[id] <= current_capacity]
+
+        # unvisited nodes, no possible customer
+        routes.append(route)
+    return routes
+
+
+def mutate():
+    pass
+
+
+def tournament_selection():
+    pass
+
+
+def crossover():
+    pass
+
+
 def run(data: Data):
-    # extract the needed data
-    capacity = data.capacity
-    depot_id = data.depot_id
+    population = []
+    tracker = []  # keep track of optimal solutions
 
-    def init():
-        """returns a randomly generated population"""
-        # scale the population size based on input
-        population_size = math.floor((data.dimension - 1) / 10) * 2
+    # scale the population size based on input
+    population_size = math.floor((data.dimension / 2))
 
-        population = []
+    # randomly generate the initial population
+    for _ in range(population_size):
+        individual = generate_individual(data)
+        cost, fixed_individual = fix_routes(data, individual)
+        population.append((cost, fixed_individual))
 
-        for _ in range(population_size):
-            unvisited = set(data.ids)  # get all customer node_ids
-            routes = []
-
-            while unvisited:
-                current_capacity = capacity
-                possible_customer = list(unvisited)
-                route = []  # path of one vehicle
-
-                while possible_customer:
-                    id = random.choice(possible_customer)  # pick a random node
-                    current_capacity -= data.demands[id]  # update truck capacity
-                    route.append(id)  # add id to path
-                    unvisited.remove(id)  # mark node as visited, remove it from set
-
-                    # update possible customers based on unvisited nodes and remaining truck capacity
-                    possible_customer = [
-                        id for id in unvisited if data.demands[id] <= current_capacity
-                    ]
-
-                # unvisited nodes, no possible customer
-                routes.append(route)
-
-            # all customers visited
-            population.append(routes)
-
-        return population
-
-    population = init()
+    return population
