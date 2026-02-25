9fc79d86508be3366847e03dc6fcfcab1948d461
UNovak <88665587+UNovak@users.noreply.github.com>
2026-02-25 03:59:22 +0100

    refactor: update main to accomodate GA changes

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 import utils.parser as parser
 from algorithms import genetic
 
-print("CVRP solver")
-
 data = parser.parse_file("./data/E-n22-k4.vrp")
-print(data)
+# print(data)
 
 # run the algorithms
-genetic.run(data)
+genetic_solution = genetic.run(data)
+print(f"GA solution={genetic_solution}")
