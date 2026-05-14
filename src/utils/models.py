2220869d89c24449c97e6d3a643984f5ead7ff82
UNovak <88665587+UNovak@users.noreply.github.com>
2026-05-14 03:02:05 +0200

    refactor!: remove Solution model

--- src/utils/models.py
+++ src/utils/models.py
@@ -1,6 +1,4 @@
-import uuid
 from dataclasses import dataclass, field
-from typing import Optional
 
 import numpy as np
 
@@ -30,21 +28,3 @@ class Data:
     nodes: np.ndarray  # size(n+1,2)
     demands: np.ndarray  # size(n+1)
     distance: np.ndarray  # size(n+1,n+1)
-
-
-@dataclass
-class Solution:
-    """
-    cost: int
-    seq: list[int]
-    algorithm: str | None
-    id: str
-    """
-
-    cost: int
-    seq: list[int]
-    algorithm: Optional[str] = None
-    id: str = field(default_factory=lambda: str(uuid.uuid4()))
-
-    def __repr__(self) -> str:
-        return f"cost={self.cost}, seq={self.seq}, algorithm={self.algorithm}"
